c52e24b484e0eff2a7d49b34a657bfe6a8b51bcd
StarTower6 <1352913877@qq.com>
2019-07-23 14:27:20 +0800

    上传界面简介模板组件 新建提交html类(未实现) 待实现图片上传到后台功能

---
+++ src/main/java/com/woos/action/CreateHtmlAction.java
@@ -0,0 +1,11 @@
+package com.woos.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/createHTML")
+@Controller
+public class CreateHtmlAction {
+
+
+}
