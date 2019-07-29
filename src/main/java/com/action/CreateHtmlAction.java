adc028c2b81ec9012d6f89348705abbcb4168e5e
StarTower6 <1352913877@qq.com>
2019-07-29 11:22:19 +0800

    将模板设计与内容编辑进行分离

--- src/main/java/com/action/CreateHtmlAction.java
+++ src/main/java/com/action/CreateHtmlAction.java
@@ -5,6 +5,7 @@ import org.omg.CORBA.StringHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.*;
@@ -20,10 +21,6 @@ public class CreateHtmlAction {
         htmltext = "<!DOCTYPE html>\n" +"<html>"+htmltext+"</html>";
         System.out.println(htmltext);
 
-        File savePathFile =new File(pageurl);
-        if (savePathFile.exists()==false){
-            savePathFile.mkdirs();
-        }
         String pageName = System.currentTimeMillis()+"";
         File file = new File(pageurl+"/"+pageName+".html");
         // 创建一个新文件
@@ -32,14 +29,8 @@ public class CreateHtmlAction {
         osw.write(htmltext);
         // 关闭输出流
         osw.close();
-        fos.close();
         System.out.println("输入完成");
-        boolean fvar = file.createNewFile();
-        if (fvar) {
-            System.out.println("File has been created successfully!");
-        } else {
-            System.out.println("File already present at the specified location!");
-        }
+
 
 
         JSONObject json =new JSONObject();
